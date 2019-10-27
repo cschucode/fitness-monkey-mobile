baa85b324e229fbd1ef5fc2edf81f314c20515a4
Kyle Lanmon <kyle.lanmon@gmail.com>
2019-10-26 23:43:15 -0700

    rebuild using react-native cli instead of create-react-native-app

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['airbnb', 'prettier'],
+  parser: 'babel-eslint',
+  plugins: ['prettier', 'react', 'jsx-a11y', 'import'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+};
--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Text style={styles.sectionTitle}>Hello world!</Text>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: '#000',
+    },
 });
+
+export default App;
